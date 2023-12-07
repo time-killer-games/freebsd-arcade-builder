6b09797551c7695fdbdd44875c0844b75eb5da11
Samuel Venable <samuelvenable@hotmail.com>
2023-12-06 22:36:57 -0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -48,9 +48,9 @@ build(){
     # Add and extract base/kernel into ${release}
     cd ${base}
     # TODO: Switch with CoreNGS release
-    fetch http://ftp.freebsd.org/pub/FreeBSD/releases/amd64/$(uname -r)/base.txz
-    fetch http://ftp.freebsd.org/pub/FreeBSD/releases/amd64/$(uname -r)/kernel.txz
-    fetch http://ftp.freebsd.org/pub/FreeBSD/releases/amd64/$(uname -r)/lib32.txz
+    fetch http://ftp.freebsd.org/pub/FreeBSD/releases/amd64/`uname -r`/base.txz
+    fetch http://ftp.freebsd.org/pub/FreeBSD/releases/amd64/`uname -r`/kernel.txz
+    fetch http://ftp.freebsd.org/pub/FreeBSD/releases/amd64/`uname -r`/lib32.txz
     tar -zxvf base.txz -C ${release}
     tar -zxvf kernel.txz -C ${release}
     tar -zxvf lib32.txz -C ${release}
