d59d7844e10cc72032aee3aba818d9be99898714
Samuel Venable <samuelvenable@hotmail.com>
2024-01-25 10:37:38 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -112,7 +112,6 @@ build(){
     cp -fR "${srcdir}/bsdinstall.desktop" ${release}/usr/home/freebsd/Desktop/bsdinstall.desktop
     chown -R freebsd ${release}/usr/home/freebsd/Desktop/bsdinstall.desktop
     chmod 755 ${release}/usr/home/freebsd/Desktop/bsdinstall.desktop
-    sudo -u freebsd -g operator dbus-launch gio set ${release}/usr/home/freebsd/Desktop/bsdinstall.desktop metadata::trusted true
     cp -fR "${srcdir}/.cpignore" ${release}/usr/home/freebsd/.cpignore
     echo "ALL ALL=(ALL:ALL) NOPASSWD: ALL" > ${release}/usr/local/etc/sudoers
     chmod 0440 ${release}/usr/local/etc/sudoers
